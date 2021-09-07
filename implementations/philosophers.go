404538c6237c7e0f806168b7a88363bb1c53795b
mille <milo@itu.dk>
2021-09-07 12:22:25 +0200

    added minimal functionality to philosofers

--- implementations/philosophers.go
+++ implementations/philosophers.go
@@ -1,1 +1,23 @@
 package implementations
+
+// each philosopher must include two channels (one for input and one for output,
+// both usable from the outside) through which it is possible to make queries on
+// the state of the philosopher (number of times eaten, eating or thinking)
+
+type Philosopher struct {
+	id          int
+	leftForkId  int
+	rightForkId int
+
+	timesEaten int
+	isEating   bool
+}
+
+func Think() {
+	//Afspil think af aretha franklin or some shit
+}
+
+func (p Philosopher) Eat() {
+	//Spis din weirdass mad, som skal spises med 2 gafler???
+	p.timesEaten++
+}
