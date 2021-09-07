e789571be0c5c5a4f5839729c55d40c66e4db703
mille <milo@itu.dk>
2021-09-07 12:49:00 +0200

    added more underwhelming functionality to philosofers

--- implementations/philosophers.go
+++ implementations/philosophers.go
@@ -5,19 +5,48 @@ package implementations
 // the state of the philosopher (number of times eaten, eating or thinking)
 
 type Philosopher struct {
-	id          int
-	leftForkId  int
-	rightForkId int
+	id        int
+	leftFork  *Fork
+	rightFork *Fork
 
 	timesEaten int
 	isEating   bool
+}
+
+// Creates a new philosopher and returns a pointer to the address
+// uses an Id, a pointer to the left fork and a pointer to the right fork
+
+func NewPhilosopher(id int, leftFork *Fork, rightFork *Fork) *Philosopher {
+	var p Philosopher
+	p.id = id
+	p.leftFork = leftFork
+	p.rightFork = rightFork
+
+	p.timesEaten = 0
+	p.isEating = false
+
+	var newPhilosopher *Philosopher = &p
+
+	return newPhilosopher
 }
 
 func Think() {
 	//Afspil think af aretha franklin or some shit
 }
 
-func (p Philosopher) Eat() {
-	//Spis din weirdass mad, som skal spises med 2 gafler???
+func Eat(p Philosopher) {
+	//[lock some shit]
+	p.isEating = true
+	//[Unlock that shit]
+	p.isEating = false
 	p.timesEaten++
+
+}
+
+func GetTimesEaten(p Philosopher) int {
+	return p.timesEaten
+}
+
+func IsEating(p Philosopher) bool {
+	return p.isEating
 }
