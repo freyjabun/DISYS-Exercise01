1a830ecb7b46a71ffab95dd916df7f79cb64eda1
SebastianVFugmann <90041204+SebastianVFugmann@users.noreply.github.com>
2021-09-07 12:28:29 +0200

    First draft at a fork implementation (#3)
    Co-authored-by: Sebastian Fugmann <sefu@itu.dk>

--- implementations/forks.go
+++ implementations/forks.go
@@ -1,1 +1,30 @@
 package implementations
+
+type Fork struct {
+	used  int
+	id    int
+	inUse bool
+}
+
+func create(id int) *Fork {
+	var fork Fork
+	fork.used = 0
+	fork.inUse = false
+	fork.id = id
+
+	var created *Fork = &fork
+
+	return created
+}
+
+func justUsed(fork Fork) {
+	fork.used++
+}
+
+func isInUse(fork Fork) bool {
+	return fork.inUse
+}
+
+func timesUsed(fork Fork) int {
+	return fork.used
+}
