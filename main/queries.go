28099044cf0e2aac175e94f5743c4be39ff6390f
Mille Mei Zhen Loo <milo@itu.dk>
2021-09-20 20:16:42 +0200

    Added out of bounds condition to queries

--- main/queries.go
+++ main/queries.go
@@ -29,12 +29,22 @@ func runCommand(command string, n int) {
 		fmt.Println("Program is exiting")
 		os.Exit(0)
 	case "inspectfork":
-		status := <-forks[number].sender
-		fmt.Println("test %v sdfsdf", status.timesUsed)
-		forks[number].reciever <- print
+		if number < count && number >= 0 {
+			status := <-forks[number].sender
+			fmt.Println("test %v sdfsdf", status.timesUsed)
+			forks[number].reciever <- print
+		} else {
+			fmt.Println("Fork number out of bounds.")
+		}
+
 	case "inspectphilosopher":
-		status := <-philosophers[number].sender
-		fmt.Printf("Philosopher with id %v has eaten %v times. Are they currently eating? %t\n", philosophers[number].id, status.timesEaten, status.isEating)
+		if number < count && number >= 0 {
+			philosophers[number].reciever <- true
+			status := <-philosophers[number].sender
+			fmt.Printf("Philosopher with id %v has eaten %v times. Are they currently eating? %t\n", philosophers[number].id, status.timesEaten, status.isEating)
+		} else {
+			fmt.Println("Philosopher number out of bounds.")
+		}
 	case "help":
 		fmt.Println("Available commands are \"exit\", \"inspectphilosopher n\", and \"inspectfork n\" where n is the id of a philosopher or fork in the range 1-5 inclusive")
 	default:
